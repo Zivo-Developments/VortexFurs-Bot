41559e8caeaa6b365c04927ccbdbac1df3fceeca
FiireWiinter <andrejpopov2003@gmail.com>
2021-09-29 01:45:32 +0200

    Kick Logging

--- src/events/MemberKick.ts
+++ src/events/MemberKick.ts
@@ -1,37 +1,37 @@
-import { GuildBan, MessageEmbed } from "discord.js";
+import { GuildMember, MessageEmbed } from "discord.js";
 import FuzzyClient from "../lib/FuzzyClient";
 import { GuildRepo } from "../repositories/GuildRepository";
 import BaseEvent from "../structures/BaseEvent";
 import { channelResolver } from "../utils/resolvers";
 
-export default class MemberBanEvent extends BaseEvent {
+export default class MemberKickEvent extends BaseEvent {
 	constructor(client: FuzzyClient) {
 		super(client, {
-			eventName: "guildBanAdd",
+			eventName: "guildMemberRemove",
 		});
 	}
-	async run(client: FuzzyClient, member: GuildBan) {
+	async run(client: FuzzyClient, member: GuildMember) {
 		const guildData = await client.database.getCustomRepository(GuildRepo).findOne({ guildID: member.guild.id });
-		if (guildData?.banLogChannelID) {
+		if (guildData?.kickLogChannelID) {
             setTimeout(async () => {
                 const fetchedLogs = await member.guild.fetchAuditLogs({
                     limit: 5,
-                    type: "MEMBER_BAN_ADD",
+                    type: "MEMBER_KICK",
                 });
-                const auditLog = fetchedLogs.entries.find((entry: any) => entry.target.id === member.user.id);
-                const banChannel = await channelResolver(client, guildData.banLogChannelID);
+                const auditLog = fetchedLogs.entries.find((entry: any) => entry.target.id === member.id);
+                const kickChannel = await channelResolver(client, guildData.kickLogChannelID);
                 
-                if (banChannel && banChannel.isText()) {
+                if (kickChannel && kickChannel.isText()) {
                     const embed = new MessageEmbed()
 					.setAuthor(`${member.user.tag} (${member.user.id})`, member.user.displayAvatarURL({ dynamic: true }))
 					.setColor("RED")
-					.setTitle("A Member has been banned from the Guild!")
+					.setTitle("A Member has been kicked from the Guild!")
 					.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-					.addField("Ban Reason", member.reason ? member.reason : "None Specified")
+					.addField("Kick Reason", auditLog?.reason ? auditLog.reason : "None Specified")
 					.addField("Issuer", `${auditLog?.executor?.tag}`)
 					.setTimestamp()
 					.setFooter(`User ID: ${member.user.id}`);
-                    banChannel.send({ embeds: [embed] });
+                    kickChannel.send({ embeds: [embed] });
                 }
             }, 1000)
         }
