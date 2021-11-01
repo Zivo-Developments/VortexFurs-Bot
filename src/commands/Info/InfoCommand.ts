e88a27f141b203a2ef03478ad562828090fff610
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-10-31 21:53:53 -0500

    Update info command

--- src/commands/Info/InfoCommand.ts
+++ src/commands/Info/InfoCommand.ts
@@ -16,13 +16,13 @@ export default class PingCommand extends BaseSlashCommand {
     async run(interaction: CommandInteraction) {
         const embed = new MessageEmbed()
             .setTitle("Information")
-            .setDescription("Discord Bot made for Frenzy Furs!")
-            .addField("GitHub", "Repository is Private at the moment 🙂")
+            .setDescription(`Discord Bot made for Frenzy Furs!\nDev Team:\n<@852070153804972043> (Lead Dev)\n<@315098355913064461> <@811393103881306123> (Developers)`)
+            .addField("GitHub (BOT)", "https://github.com/Frenzy-Furs/Bot")
+            .addField("GitHub (WEB)", "Coming Soon")
             .addField("Bugs/Feature Request", `Please DM ${interaction.client.users.cache.get("852070153804972043")}!`)
-            .addField("When will it be Public?", "We don't plan to make it public at the moment!")
             .setColor("#ff1493"!)
             .setThumbnail(interaction.guild?.iconURL({ dynamic: true })!)
-            .setFooter(`Bot made by ${interaction.client.users.cache.get("852070153804972043")!.tag}`);
+            .setFooter(`Bot made by Frenzy Furs Development Team`);
         interaction.reply({ embeds: [embed] });
     }
 }
