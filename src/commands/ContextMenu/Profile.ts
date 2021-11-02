3980d28635ff24b71c916f8701aca5a2e7d8084a
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-11-02 18:37:49 -0500

    Profile Context Menu Fix

--- src/commands/ContextMenu/Profile.ts
+++ src/commands/ContextMenu/Profile.ts
@@ -17,25 +17,25 @@ export default class PingCommand extends BaseSlashCommand {
         const memberRepo = this.client.database.getCustomRepository(MemberRepo);
         const guild = this.client.guilds.cache.get(this.client.config.guildID);
         const member = interaction.guild?.members.cache.get(interaction.targetId)!;
-        const profile = await memberRepo.findOne({ guildID: this.client.config.guildID, userID: interaction.user.id });
+        const profile = await memberRepo.findOne({ guildID: this.client.config.guildID, userID: member.user.id });
         if (!profile)
             throw new Error(
                 `${member.user.username} doesnt't have a profile! They need to create one in https://www.hozol.xyz/profile/create`,
             );
         const embed = new MessageEmbed()
-            .setAuthor(interaction.user.username, interaction.user.displayAvatarURL())
-            .setTitle(`Profile Card - ${interaction.user.username}`)
+            .setAuthor(member.user.username, member.user.displayAvatarURL())
+            .setTitle(`Profile Card - ${member.user.username}`)
             .setDescription(`Bio: ${profile.bio ? profile.bio : "No Bio"}`)
             .addField(
                 "Roles",
-                (interaction.member as unknown as GuildMember)!.roles.cache
+                (member.user as unknown as GuildMember)!.roles.cache
                     .sort((a: Role, b: Role) => b.position - a.position)
                     .map((r) => r)
                     .join(" "),
             )
             .addField("More Stuff?", "Coming Soon")
             .setColor("#ff1493"!)
-            .setURL("https://www.hozol.xyz/profile/")
+            .setURL(`https://www.hozol.xyz/profile/${member.id}`)
             .setThumbnail(interaction.user?.avatarURL({ dynamic: true })!);
         interaction.reply({ embeds: [embed], ephemeral: true });
     }
