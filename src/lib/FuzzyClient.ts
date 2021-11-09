2bd007a0404a0066e15e108f425906bbafe0b41f
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-11-08 18:44:39 -0600

    Leveling Stuff *untested*

--- src/lib/FuzzyClient.ts
+++ src/lib/FuzzyClient.ts
@@ -21,6 +21,7 @@ export default class FuzzyClient extends Client {
     arrayOfSlashCommands: (BaseCommand & ApplicationCommandData)[];
     scheduleRepo: ScheduleRepo;
     scheduleManager: ScheduleManager;
+    xpCooldown: string[];
     utils: Utils;
     config: typeof config;
     database: Connection;
@@ -32,6 +33,7 @@ export default class FuzzyClient extends Client {
         this.furryAPI = new Yiffy();
         this.schedules = {};
         this.arrayOfSlashCommands = [];
+        this.xpCooldown = [];
         this.utils = new Utils();
         this.scheduleManager = new ScheduleManager(this);
         this._logger = new Logger(this);
